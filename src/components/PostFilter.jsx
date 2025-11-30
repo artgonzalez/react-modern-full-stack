89cf1476f211fbd4212cbd14950b9a3638ecf320
artgonzalezacc <arturo.gonzalez@austincc.edu>
2025-11-29 21:18:06 -0600

    fix issue

--- src/components/PostFilter.jsx
+++ src/components/PostFilter.jsx
@@ -1,32 +1,22 @@
 import PropTypes from 'prop-types'
-import { memo } from 'react';
 
-// Use memo for performance optimization since this is a simple, potentially frequently used input
-export const PostFilter = memo(({ label, value, onChange }) => {
-    // Generate a unique ID for accessibility best practices (label htmlFor matches input id)
-    const inputId = `filter-${label.toLowerCase().replace(/\s/g, '-')}`;
-
+export function PostFilter({ field, value, onChange }) {
     return (
-        <div>
-            <label htmlFor={inputId}>{label}: </label>
-            <input
-                type='text'
-                name={inputId}
-                id={inputId}
-                value={value}
-                onChange={(e) => onChange(e.target.value)}
-                placeholder={`Filter by ${label}`}
-            />
-        </div>
-    );
-});
+      <div>
+        <label htmlFor={`filter-${field}`}>{field}: </label>
+        <input
+          type='text'
+          name={`filter-${field}`}
+          id={`filter-${field}`}
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+        />
+      </div>
+    )
+};
 
-// Define PropTypes for type checking
 PostFilter.propTypes = {
-    label: PropTypes.string.isRequired,
+    field: PropTypes.string.isRequired,
     value: PropTypes.string.isRequired,
     onChange: PropTypes.func.isRequired,
 };
-
-// Define displayName for memoized components in React DevTools
-PostFilter.displayName = 'PostFilter';
