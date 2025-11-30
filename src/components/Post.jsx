61e041e73fe0a5a09ab95e5365dbfbec8adacf5d
artgonzalezacc <arturo.gonzalez@austincc.edu>
2025-11-29 21:05:44 -0600

    refactor best practice

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,23 +1,28 @@
 import PropTypes from 'prop-types'
 import { User } from './User.jsx'
 
+// Destructure props explicitly in the function signature
 export function Post({ title, contents, author }) {
     return (
+        // Using <article> is good practice for self-contained content
         <article>
-        <h3>{title}</h3>
-        <div>{contents}</div>
-        {author && (
-        <em>
-        <br />
-        Written by <strong><User id={author} /></strong>
-        </em>
-        )}
+            <h3>{title}</h3>
+            <div>{contents}</div>
+            
+            {/* Conditionally render the footer if an author ID is provided */}
+            {author && (
+                // Use a footer for post metadata like author info
+                <footer>
+                    Written by <strong><User id={author} /></strong>
+                </footer>
+            )}
         </article>
-    )
-};
+    );
+}
 
+// Define PropTypes for type checking
 Post.propTypes = {
     title: PropTypes.string.isRequired,
-    contents: PropTypes.string,
-    author: PropTypes.string,
+    contents: PropTypes.string, // Contents is optional
+    author: PropTypes.string,   // Author ID is optional (e.g., for anonymous posts)
 };
